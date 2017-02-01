dd56d9bcbba628cabd30a191db3743d20ad0d4b9
Cphil31 <cecile_philippe@live.fr>
2017-02-01 13:54:00 +0100

    step2 fait

--- index_pdo2.php
+++ index_pdo2.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php'; 
 ORM::configure('mysql:host=localhost;dbname=mon_armoire');
 ORM::configure('username','root');
 ORM::configure('password','root');
